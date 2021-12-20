99efde5a4669e21bd5381f761790f07c831a18ff
Oleh Didyk <oleggsa@gmail.com>
2021-12-20 10:06:56 +0200

    added parallel & series exports

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 
 const copyIndex = () => {
+  console.log('first task');
   return src('./dist/pages/index.html')
     .pipe(dest('build/pages'));
 };
 
+const copySecond = () => {
+  console.log('second task');
+  return src('./dist/pages/second-page.html')
+    .pipe(dest('build/pages'));
+};
+
 const copyScss = () => {
+  console.log('third task');
   return src('./dist/scss/common/*.scss')
     .pipe(dest('./build/styles'));
 };
 
 exports.copy = copyIndex;
 exports.default = copyScss;
+exports.parallel = parallel(copyIndex, copySecond, copyScss);
+exports.series = series(copyIndex, copySecond, copyScss);
