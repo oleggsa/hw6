c4b9af0c403189fe437c6bb038a7a7b10dc20775
Oleh Didyk <oleggsa@gmail.com>
2021-12-20 09:56:33 +0200

    globs added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const firstTask = (done) => {
-    console.log('My First Hexlet Task');
-  
-    done();
-  };
-  
-  exports.default = firstTask;
+const { src, dest, parallel, series, watch } = require('gulp');
+
+const copyIndex = () => {
+  return src('./dist/pages/index.html')
+    .pipe(dest('build/pages'));
+};
+
+const copyScss = () => {
+  return src('./dist/scss/common/*.scss')
+    .pipe(dest('./build/styles'));
+};
+
+exports.copy = copyIndex;
+exports.default = copyScss;
